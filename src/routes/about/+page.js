6a0f17ca1521abac70554af4caff33ad0a0269f3
iamBijoyKar <bijoypoco12345@gmail.com>
2023-12-12 21:37:03 +0530

    [add]: about page added

---
+++ src/routes/about/+page.js
@@ -0,0 +1,11 @@
+export const prerender = true;
+export const ssr = true;
+
+// export async function load() {
+//   fetch(`https://api.github.com/repos/TechStarHub/svelte-typer-svelte-kit`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       //   console.log(data); // Prints result from `response.json()` in getRequest
+//       return { data };
+//     });
+// }
