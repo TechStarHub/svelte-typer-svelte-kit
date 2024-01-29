50b40852ed62cae5de85db8e154c4ee003dff6d0
Bijoy Kar <bijoykar54321@gmail.com>
2024-01-29 21:41:09 +0530

    [update]: Navbar added in sm devices 📱

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,9 +1,11 @@
 <script>
     import NavBar from "./NavBar.svelte";
+    import SmNavBar from "./SmNavBar.svelte";
     import Brandlogo from "../Brandlogo.svelte";
 </script>
 
-<header class="w-full px-6 py-4 mb-8 flex justify-between items-center " >
+<header class="w-full px-6 py-4 mb-8 flex gap-3 sm:justify-between items-center " >
+    <SmNavBar />
     <a href="/" class="">
         <Brandlogo />
     </a>
--- src/lib/components/Header/NavBar.svelte
+++ src/lib/components/Header/NavBar.svelte
@@ -2,7 +2,7 @@
     import NavLikn from "./NavLikn.svelte";
 </script>
 
-<nav class=" font-sans">
+<nav class=" font-sans hidden sm:block">
     <ul class="flex gap-4">
         <NavLikn fillIcon="ri:home-fill" lineIcon="ri:home-line" link="/" label="Home" />
         <NavLikn fillIcon="material-symbols:keyboard" lineIcon="material-symbols:keyboard-outline" link="/typer" label="Typer" />
