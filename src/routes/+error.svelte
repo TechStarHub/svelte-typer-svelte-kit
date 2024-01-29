3e172532942a4abc53df5546e1053d7a3253f3ff
Bijoy Kar <bijoykar54321@gmail.com>
2024-01-29 21:56:46 +0530

    [update]: Error page ui updated ✨

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,11 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status}: {$page.error.message}</h1>
+<div class="flex flex-col items-center gap-1">
+	<img src="/error-404.png" class="" width="300" alt="">
+	<h1 class="text-2xl font-bold text-slate-800 mt-1">Error {$page.status}: Page {$page.error.message}</h1>
+	<p class="max-w-[500px] text-center mt-1 text-lg">
+		There was an error loading this page. Please try again later.
+	</p>
+	<a href="/" class="text-blue-600 font-medium hover:underline">Go to home page</a>
+</div>
