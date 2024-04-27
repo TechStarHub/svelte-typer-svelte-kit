c1b59d1c99d5a4c6c99ac9cc181a398a7808abe3
Bijoy Kar <bijoykar54321@gmail.com>
2024-04-27 20:45:51 +0530

    [add]: Added new page for words in typer route

---
+++ src/routes/typer/words/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+    onMount(() => {
+        if (page.status === 404) {
+            goto('/404');
+        }
+    });
+</script>
+
+<div class="">
+    This is the words page
+</div>
